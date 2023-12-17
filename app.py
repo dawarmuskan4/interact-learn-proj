c6fffaf163949b5319b856d824d3440ad8ff78e6
muskan-uwc <muskan@unitedwecare.com>
2023-12-18 00:22:11 +0530

    web page updated-6

--- app.py
+++ app.py
@@ -43,35 +43,32 @@ def main():
     elif dataset_name != 'None':
         get_sidebar(dataset_name)
     else:
-    # Home
-    st.markdown("""
-                    <h1 style='text-align: center;'>\
-                        Interact Learn</h1>
-                    """, 
-                    unsafe_allow_html=True)
-        
-    gif_url = "https://media.giphy.com/media/A0B7BnpAVRjMJYBZWD/giphy.gif"
-    st.markdown(
-        
-        f'<div style="display: flex; justify-content: center; align-items: center; height: 60vh;">'
-        f'<img src="{gif_url}" width="500" style="object-fit: contain;">'
-        f'</div>',
-        unsafe_allow_html=True
+        # Home
+        st.markdown("""
+            <h1 style='text-align: center;'>Interact Learn</h1>
+            """,
+            unsafe_allow_html=True)
 
-    
-    st.markdown(
-        """
-        <div style="font-size: 24px; text-align: center;">
-            Project by: 
-            <img src="https://media.giphy.com/media/HQTYdpx1yhxWpugAi2/giphy.gif" alt="Project Gif" width="70" height="70">
-            <a href="https://www.linkedin.com/in/sujalsethi44/" target="_blank">Sujal Sethi</a>, 
-            <a href="https://www.linkedin.com/in/dawarmuskan4/" target="_blank">Muskan Dawar</a>, 
-            <a href="https://www.linkedin.com/in/nimish-batra/" target="_blank">Nimish Batra</a>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+        gif_url = "https://media.giphy.com/media/A0B7BnpAVRjMJYBZWD/giphy.gif"
+        st.markdown(
+            f'<div style="display: flex; justify-content: center; align-items: center; height: 60vh;">'
+            f'<img src="{gif_url}" width="500" style="object-fit: contain;">'
+            f'</div>',
+            unsafe_allow_html=True
+        )
 
+        st.markdown(
+            """
+            <div style="font-size: 24px; text-align: center;">
+                Project by: 
+                <img src="https://media.giphy.com/media/HQTYdpx1yhxWpugAi2/giphy.gif" alt="Project Gif" width="70" height="70">
+                <a href="https://www.linkedin.com/in/sujalsethi44/" target="_blank">Sujal Sethi</a>, 
+                <a href="https://www.linkedin.com/in/dawarmuskan4/" target="_blank">Muskan Dawar</a>, 
+                <a href="https://www.linkedin.com/in/nimish-batra/" target="_blank">Nimish Batra</a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
 
 if __name__ == '__main__':
